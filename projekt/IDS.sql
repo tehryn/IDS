36fe99165ea230b197d3c04c9eb31a2cd5b91d9d
tehryn <thetehryn@gmail.com>
2017-03-25 12:03:19 +0100

    SQL script

---
+++ projekt/IDS.sql
@@ -0,0 +1,215 @@
+DROP TABLE Zamestnanec CASCADE CONSTRAINTS;
+DROP TABLE Rezervace   CASCADE CONSTRAINTS;
+DROP TABLE Objednavka  CASCADE CONSTRAINTS;
+DROP TABLE Potravina   CASCADE CONSTRAINTS;
+DROP TABLE Stul        CASCADE CONSTRAINTS;
+DROP TABLE Surovina    CASCADE CONSTRAINTS;
+DROP TABLE OObsahujeP  CASCADE CONSTRAINTS;
+DROP TABLE PObsahujeS  CASCADE CONSTRAINTS;
+DROP TABLE RObsahujeS  CASCADE CONSTRAINTS;
+DROP TABLE Uctenka     CASCADE CONSTRAINTS;
+
+ALTER SESSION SET NLS_DATE_FORMAT = 'dd.mm.yyyy, hh24:mi';
+
+CREATE TABLE Zamestnanec ( /**/
+  Id           INTEGER PRIMARY KEY,
+  prac_pozice VARCHAR(32) NOT NULL,
+  jmeno       VARCHAR(64) NOT NULL,
+  rod_cislo   INTEGER     NOT NULL,
+  kontakt     VARCHAR(128),
+  cislo_uctu  INTEGER,
+  plat        INTEGER     NOT NULL
+);
+
+CREATE TABLE Rezervace ( /**/
+  ID          INTEGER PRIMARY KEY,
+  cas         DATE         NOT NULL,
+  jmeno_obj   VARCHAR(64)  NOT NULL,
+  kontakt_obj VARCHAR(128) NOT NULL,
+  jmeno_zak   VARCHAR(64),
+  kontakt_zak VARCHAR(128),
+  ID_zam      INTEGER      NOT NULL REFERENCES Zamestnanec(ID)
+);
+
+CREATE TABLE Stul ( /**/
+  cislo       INTEGER PRIMARY KEY,
+  lokace      VARCHAR(32),
+  pocet_zidli INTEGER
+);
+
+CREATE TABLE RObsahujeS (
+  id_rezervace INTEGER NOT NULL REFERENCES Rezervace(ID),
+  cislo_stolu  INTEGER NOT NULL REFERENCES Stul(cislo)
+);
+
+CREATE TABLE Objednavka ( /**/
+  ID              INTEGER PRIMARY KEY,
+  cas             DATE NOT NULL,
+  poznamka        VARCHAR(512)
+);
+
+CREATE TABLE Potravina ( /**/
+  jmeno          VARCHAR(128) PRIMARY KEY,
+  druh           VARCHAR(32) NOT NULL,
+  doba_pripravy  INTEGER,
+  popis_pripravy VARCHAR(2048),
+  cena           FLOAT NOT NULL,
+  prodano_kusu   INTEGER NOT NULL,
+  talir          VARCHAR(32),
+  sklenice       VARCHAR(32)
+);
+
+CREATE TABLE OObsahujeP ( /**/
+  ID     INTEGER       NOT NULL  REFERENCES Objednavka(ID),
+  jmeno  VARCHAR(128)  NOT NULL  REFERENCES Potravina(jmeno),
+  mnozstvi INTEGER     NOT NULL
+);
+
+CREATE TABLE Surovina ( /**/
+  jmeno VARCHAR(128) PRIMARY KEY,
+  alergeny VARCHAR(64)
+);
+
+CREATE TABLE PObsahujeS ( /**/
+  jmenoP VARCHAR(128) NOT NULL REFERENCES Potravina(jmeno),
+  jmenoS VARCHAR(128) NOT NULL REFERENCES Surovina(jmeno),
+  mnozstvi INTEGER NOT NULL
+);
+
+CREATE TABLE Uctenka (
+  ID         INTEGER  PRIMARY KEY,
+  datum      DATE     NOT NULL,
+  zaloha     INTEGER,
+  suma       INTEGER  NOT NULL,
+  objednavka INTEGER
+);
+
+INSERT INTO Zamestnanec VALUES (
+    01, 'kuchař', 'Ladislav Hruštička', 6611111166 , '+420 565 535 879', 1234567890100, 15000
+);
+INSERT INTO Zamestnanec VALUES (
+    02, 'cisnik', 'Uršula Jablíčková', 8012241811, '+420 603 214 715', 7359658920100, 12000
+);
+INSERT INTO Zamestnanec VALUES (
+    03, 'barman', 'Igor Třešnička', 9302291666, '+420 725 735 615', 6782135480200, 14500
+);
+
+INSERT INTO Stul VALUES (
+  1, 'Zaharada', 6
+);
+INSERT INTO Stul VALUES (
+  2, 'Kuřácký sál', 8
+);
+INSERT INTO Stul VALUES (
+  3, 'Nekuřácký sál', 4
+);
+
+INSERT INTO Rezervace VALUES (
+  0, '01.01.2017, 10:30', 'Filip Kachnička', '603 305 452', NULL , NULL , 01
+);
+INSERT INTO Rezervace VALUES (
+  1, '04.01.2017, 12:00', 'Alexandr Malý', '714 413 725', NULL , NULL , 01
+);
+INSERT INTO Rezervace VALUES (
+  2, '04.01.2017, 12:00', 'Firma s.r.o', '603 305 452', 'Dagmar Štýrská' , 'dgstyrska@suznam.org' , 02
+);
+
+INSERT INTO RObsahujeS VALUES (
+  0, 1
+);
+INSERT INTO RObsahujeS VALUES (
+  1, 1
+);
+INSERT INTO RObsahujeS VALUES (
+  2, 2
+);
+INSERT INTO RObsahujeS VALUES (
+  2, 3
+);
+
+INSERT INTO Potravina VALUES (
+    'Vepřový steak', 'Vepřové', 90, 'Hodit flákotu masa na pánev a osmažit', 90, 0, 'Mělký talíř', NULL
+);
+INSERT INTO Potravina VALUES (
+    'Pivo', 'Alkohol', NULL, NULL, 25, 0, NULL, 'Půllitr'
+);
+INSERT INTO Potravina VALUES (
+    'Malinovka', 'Nealko', NULL, NULL, 15, 0, NULL, 'Třetinka'
+);
+
+INSERT INTO Surovina VALUES (
+    'Malinová limonáda', NULL
+);
+INSERT INTO Surovina VALUES (
+    'vepřová krkovice', NULL
+);
+INSERT INTO Surovina VALUES (
+    'Plze�? 12', NULL
+);
+INSERT INTO Surovina VALUES (
+    'mléko', '7' /* muze byt i 7, 1, ...*/
+);
+
+INSERT INTO PObsahujeS VALUES (
+    'Vepřový steak', 'vepřová krkovice', 200 /*gram*/
+);
+INSERT INTO PObsahujeS VALUES (
+    'Pivo', 'Plze�? 12', 5 /*deci*/
+);
+INSERT INTO PObsahujeS VALUES (
+    'Malinovka', 'Malinová limonáda', 3 /*deci*/
+);
+
+INSERT INTO Objednavka VALUES (
+    0, '01.01.2017, 10:00', NULL
+);
+INSERT INTO Objednavka VALUES (
+    1, '01.01.2017, 12:00', 'Placeno stravenkou'
+);
+INSERT INTO Objednavka VALUES (
+    2, '01.01.2017, 12:30', 'Pochválen kuchař'
+);
+
+INSERT INTO OObsahujeP VALUES (
+    0, 'Vepřový steak', 1
+);
+INSERT INTO OObsahujeP VALUES (
+    0, 'Pivo', 2
+);
+INSERT INTO OObsahujeP VALUES (
+    1, 'Vepřový steak', 1
+);
+INSERT INTO OObsahujeP VALUES (
+    2, 'Vepřový steak', 2
+);
+INSERT INTO OObsahujeP VALUES (
+    2, 'Pivo', 6
+);
+
+INSERT INTO Uctenka VALUES (
+    0, '01.01.2017, 11:56', NULL, 140, 0
+);
+INSERT INTO Uctenka VALUES (
+    1, '01.01.2017, 13:15', NULL, 90, 1
+);
+
+INSERT INTO Uctenka VALUES (
+    2, '01.01.2017, 12:02', NULL, 40, 2 /* platba zálohy*/
+);
+INSERT INTO Uctenka VALUES (
+    3, '01.01.2017, 15:47', 40, 330, 2
+);
+
+/*
+COMMIT;
+SELECT * FROM Zamestnanec;
+SELECT * FROM Rezervace;
+SELECT * FROM Objednavka;
+SELECT * FROM Stul;
+SELECT * FROM Uctenka;
+SELECT * FROM Potravina;
+SELECT * FROM Surovina;
+SELECT * FROM PObsahujeS;
+SELECT * FROM RObsahujeS;
+SELECT * FROM OObsahujeP;
+*/
